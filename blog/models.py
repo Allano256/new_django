79ac258b4a38bf54b3996e2aa555df07bb576a69
Allano256 tech <allanzizinga@yahoo.com>
2024-05-28 11:16:45 +0000

    Worked on migrations

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,11 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug= models.SlugField(max_length=200, unique=True)#In publishing, a slug is a short name for an article that is still in production
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")#One user can write many posts, so this is a one to many or foreign key
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")#One user can write many posts, so this is a one to many or foreign key,
+                                                                                     #models.CASCADE: if a user is deleted, then any posts they have authored will also be deleted.
     content = models.TextField() #This is the blog article content
     created_on = models.DateTimeField(auto_now_add = True) #The auto_now_add= True, means the default created time is the time of post entry.
     status = models.IntegerField(choices=STATUS, default=0) #A draft is defined as zero and published as one, so you can see the default is to save as a draft.
+    exerpt = models.TextField(blank=True)#As the excerpt is optional, the user must be able to leave this database row blank without throwing an error.
+    updated_on = models.DateTimeField(auto_now=True) #The auto_now argument for the updated_on field sets the value to the current date and time whenever the record is saved, not just when it is created.
 
