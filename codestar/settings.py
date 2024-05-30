f6a756fa7bfad3be8c84a9af01a2723eb451a7a3
Allano256 tech <allanzizinga@yahoo.com>
2024-05-30 07:05:30 +0000

    Created previous button

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['8000-allano256-blogpost-njmyd159ca1.ws-eu114.gitpod.io','.herokuapp.com']
